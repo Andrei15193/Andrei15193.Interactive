240820ff34304a8855b959e7e3d921a8902f1294
Andrei Fangli <andrei_fangli@outlook.com>
2016-04-28 17:22:24 +0300

    Added default implementation to INotifyPropertyChanged.

---
+++ Andrei15193.Interactive.Tests/PropertyChangedNotifierTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Andrei15193.Interactive.Tests
+{
+    [TestClass]
+    public class PropertyChangedNotifierTests
+    {
+        private sealed class MockPropertyChangedNotifier
+            : PropertyChangedNotifier
+        {
+            new public void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                base.NotifyPropertyChanged(propertyName);
+            }
+        }
+
+        private MockPropertyChangedNotifier PropertyChangedNotifier { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            PropertyChangedNotifier = new MockPropertyChangedNotifier();
+        }
+
+        [TestMethod]
+        public void TestPropertyChangedRaisesEvent()
+        {
+            var raiseCount = 0;
+
+            PropertyChangedNotifier.PropertyChanged +=
+                delegate
+                {
+                    raiseCount++;
+                };
+            PropertyChangedNotifier.NotifyPropertyChanged("test");
+
+            Assert.AreEqual(1, raiseCount);
+        }
+
+        [TestMethod]
+        public void TestSenderIsSameWithNotifier()
+        {
+            object expectedSender = PropertyChangedNotifier;
+            object actualSender = null;
+            PropertyChangedNotifier.PropertyChanged +=
+                (sender, e) =>
+                {
+                    actualSender = sender;
+                };
+
+            PropertyChangedNotifier.NotifyPropertyChanged("test");
+
+            Assert.AreSame(expectedSender, actualSender);
+        }
+
+        [TestMethod]
+        public void TestPropertyNameIsEqualToTheNotifiedOne()
+        {
+            string expectedPropertyName = "test";
+            string actualPropertyName = null;
+            PropertyChangedNotifier.PropertyChanged +=
+                (sender, e) =>
+                {
+                    actualPropertyName = e.PropertyName;
+                };
+
+            PropertyChangedNotifier.NotifyPropertyChanged(expectedPropertyName);
+
+            Assert.AreEqual(expectedPropertyName, actualPropertyName, ignoreCase: false);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestCannotRaiseEventWithNullPropertyName()
+            => PropertyChangedNotifier.NotifyPropertyChanged(null);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCannotRaiseEventWithEmptyStringPropertyName()
+            => PropertyChangedNotifier.NotifyPropertyChanged(string.Empty);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestCannotRaiseEventInvalidIdentifierAsPropertyName()
+            => PropertyChangedNotifier.NotifyPropertyChanged("1");
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPropertyNameIsNotTrimmed()
+            => PropertyChangedNotifier.NotifyPropertyChanged(" test ");
+    }
+}
---
+++ Andrei15193.Interactive/PropertyChangedNotifier.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+
+namespace Andrei15193.Interactive
+{
+    /// <summary>
+    /// Represents a default implementation of the <see cref="INotifyPropertyChanged"/>
+    /// interface.
+    /// </summary>
+    public class PropertyChangedNotifier
+        : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged"/> event for the given property name.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The name of the property that has changed.
+        /// </param>
+        /// <exception cref="ArgumentNullException">
+        /// Thrown when <paramref name="propertyName"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// Thrown when <paramref name="propertyName"/> is an invalid identifier.
+        /// </exception>
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+            if (!Regex.IsMatch(propertyName, "^[_a-z][_a-z0-9]*$", RegexOptions.IgnoreCase))
+                throw new ArgumentException("Must be a valid identifier", nameof(propertyName));
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
