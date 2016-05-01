c37ea160447c5a0ef01e5eac04fb0709fec2d708
Andrei Fangli <andrei_fangli@outlook.com>
2016-05-01 23:14:33 +0300

    Added Windows App test app.

---
+++ Andrei15193.Interactive.Tests.Windows/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Andrei15193.Interactive.Tests.Windows
+{
+    public sealed partial class MainPage
+        : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Andrei15193.Interactive.Tests.Windows/TestViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+
+namespace Andrei15193.Interactive.Tests.Windows
+{
+    public class TestViewModel
+        : ViewModel<object>
+    {
+        public TestViewModel()
+            : base(new object())
+        {
+            GoToState1Command = GetTransitionCommand("State1");
+            GoToState2Command = GetTransitionCommand("State2");
+        }
+
+        public ICommand GoToState1Command { get; }
+        public ICommand GoToState2Command { get; }
+    }
+}
