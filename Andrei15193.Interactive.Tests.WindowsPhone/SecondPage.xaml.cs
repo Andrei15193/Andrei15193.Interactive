c437925116428d1f933ed8e65b5c9e7b9e141002
Andrei Fangli <andrei_fangli@outlook.com>
2016-05-08 12:18:57 +0300

    Changed WindowsPhone test app use content dialog page as second page.

--- Andrei15193.Interactive.Tests.WindowsPhone/SecondPage.xaml.cs
+++ Andrei15193.Interactive.Tests.WindowsPhone/SecondPage.xaml.cs
@@ -20,7 +20,7 @@ namespace Andrei15193.Interactive.Tests.WindowsPhone
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SecondPage : Page
+    public sealed partial class SecondPage : ContentDialogPage
     {
         public SecondPage()
         {
