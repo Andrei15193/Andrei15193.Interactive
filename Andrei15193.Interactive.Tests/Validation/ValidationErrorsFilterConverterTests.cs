8c619cac87181a2f7071578a093e58519b36d515
Andrei Fangli <andrei_fangli@outlook.com>
2016-05-13 00:07:36 +0300

    Added filter converter for ValidationErrors.

---
+++ Andrei15193.Interactive.Tests/Validation/ValidationErrorsFilterConverterTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Andrei15193.Interactive.Validation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Windows.UI.Xaml.Data;
+
+namespace Andrei15193.Interactive.Tests.Validation
+{
+    [TestClass]
+    public class ValidationErrorsFilterConverterTests
+    {
+        private static IValueConverter Converter { get; } = new ValidationErrorsFilterConverter();
+        private static ValidationError validationError1 { get; } = new ValidationError("test error 1");
+        private static ValidationError validationError2 { get; } = new ValidationError("test error 2", "test member 1");
+        private static ValidationError validationError3 { get; } = new ValidationError("test error 3", "test member 2");
+        private static IEnumerable<ValidationError> validationErrors { get; } =
+            new[]
+            {
+                validationError1,
+                validationError2,
+                validationError3
+            };
+
+        [TestMethod]
+        public void TestConvertingNullReturnsNull()
+        {
+            var result = Converter.Convert(null, typeof(object), null, null);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void TestFilterValidationErrorsWithNullParameter()
+        {
+            var result = Convert(null);
+
+            Assert.IsTrue(
+                new[]
+                {
+                    validationError1
+                }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void TestFilterValidationErrorsWithStringParameter()
+        {
+            var result = Convert(validationError2.MemberName);
+
+            Assert.IsTrue(
+                new[]
+                {
+                    validationError2
+                }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void TestFilterValidationErrorsWithStringCollectionParameter()
+        {
+            var result = Convert(new[] { validationError2.MemberName, validationError3.MemberName });
+
+            Assert.IsTrue(
+                new[]
+                {
+                    validationError2,
+                    validationError3
+                }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        public void TestFilterValidationErrorsWithStringCollectionParameterContainingAlsoNull()
+        {
+            var result = Convert(new[] { validationError2.MemberName, validationError3.MemberName, null });
+
+            Assert.IsTrue(
+                new[]
+                {
+                    validationError1,
+                    validationError2,
+                    validationError3
+                }.SequenceEqual(result));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void TestConvertBackIsNotImplemented()
+            => Converter.ConvertBack(null, null, null, null);
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestWithInvalidParameterTypeThrowsException()
+            => Convert(default(int));
+
+        private static IEnumerable<ValidationError> Convert(object paramter)
+            => (IEnumerable<ValidationError>)Converter.Convert(validationErrors, typeof(object), paramter, null);
+    }
+}
---
+++ Andrei15193.Interactive/Validation/ValidationErrorsFilterConverter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Windows.UI.Xaml.Data;
+
+namespace Andrei15193.Interactive.Validation
+{
+    public class ValidationErrorsFilterConverter
+        : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return null;
+
+            if (targetType == null)
+                throw new ArgumentNullException(nameof(targetType));
+            if (!targetType.GetTypeInfo().IsAssignableFrom(typeof(IEnumerable<ValidationError>).GetTypeInfo()))
+                throw new ArgumentException($"Result cannot be converted to {nameof(targetType)}.", nameof(targetType));
+            var validationErrors = (IEnumerable<ValidationError>)value;
+
+            if (parameter == null)
+                return from validationError in validationErrors
+                       where validationError?.MemberName == null
+                       select validationError;
+
+            var memberName = parameter as string;
+            if (memberName != null)
+                return from validationError in validationErrors
+                       where StringComparer.OrdinalIgnoreCase.Equals(memberName, validationError?.MemberName)
+                       select validationError;
+
+            var memberNames = parameter as IEnumerable<string>;
+            if (memberNames != null)
+                return from validationError in validationErrors
+                       where memberNames.Contains(validationError?.MemberName, StringComparer.OrdinalIgnoreCase)
+                       select validationError;
+
+            throw new ArgumentException($"The value provided to {nameof(parameter)} must be null, a string or a collection of strings.", nameof(parameter));
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
