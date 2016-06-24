2e2813816d10c9bea4c3fe8013ca7d528616cb24
Andrei Fangli <andrei_fangli@outlook.com>
2016-06-24 21:21:36 +0300

    Removed Interactive.ViewModel and added Interactive.VisualState in order to bind to and have the VisualState change as the InteractiveViewModel changes its State.

--- Andrei15193.Interactive/Interactive.cs
+++ Andrei15193.Interactive/Interactive.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -9,76 +7,92 @@ namespace Andrei15193.Interactive
 {
     public static class Interactive
     {
-        public static readonly DependencyProperty ViewModelProperty =
-            DependencyProperty.RegisterAttached("ViewModel",
-                typeof(InteractiveViewModel),
-                typeof(Interactive),
-                new PropertyMetadata(null, ViewModelChanged));
-
-        private static void ViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            var control = d as Control;
-            if (control != null)
-            {
-                var interactiveViewModel = e.OldValue as InteractiveViewModel;
-                if (interactiveViewModel != null)
-                {
-                    interactiveViewModel.PropertyChanged -= GetPropertyChangedEventHandler(control, interactiveViewModel);
-                    control.Loaded -=
-                        delegate
-                        {
-                            GoToCurrentState(control, interactiveViewModel);
-                        };
-                }
-
-                interactiveViewModel = e.NewValue as InteractiveViewModel;
-                if (interactiveViewModel != null)
-                {
-                    interactiveViewModel.PropertyChanged += GetPropertyChangedEventHandler(control, interactiveViewModel);
-                    control.Loaded +=
-                        delegate
-                        {
-                            Debug.WriteLine("Control loaded.");
-                            GoToCurrentState(control, interactiveViewModel);
-                        };
-                    GoToCurrentState(control, interactiveViewModel);
-                }
-            }
-        }
-
-        private static PropertyChangedEventHandler GetPropertyChangedEventHandler(Control control, InteractiveViewModel interactiveViewModel)
-            => (sender, e) =>
-            {
-                if (nameof(interactiveViewModel.State).Equals(e.PropertyName, StringComparison.OrdinalIgnoreCase))
-                    GoToCurrentState(control, interactiveViewModel);
-            };
-
-        private static void GoToCurrentState(Control control, InteractiveViewModel interactiveViewModel)
-        {
-            var viewModelState = interactiveViewModel.State;
-            if (!VisualStateManager.GoToState(control, viewModelState, GetUseTransitions(control)))
-                Debug.WriteLine($"Could not navigate to {viewModelState} state [SynchronizationContext: {SynchronizationContext.Current?.ToString() ?? "<null>"}].");
-        }
-
-        public static InteractiveViewModel GetViewModel(DependencyObject dependencyObject)
-            => (InteractiveViewModel)dependencyObject.GetValue(ViewModelProperty);
-        public static void SetViewModel(DependencyObject dependencyObject, InteractiveViewModel interactiveViewModel)
-            => dependencyObject.SetValue(ViewModelProperty, interactiveViewModel);
-
-        public static DependencyProperty UseTransitionsProperty { get; } =
+        public static readonly DependencyProperty UseTransitionsProperty =
             DependencyProperty.RegisterAttached("UseTransitions",
                 typeof(bool),
                 typeof(Interactive),
                 new PropertyMetadata(true));
 
         public static bool GetUseTransitions(DependencyObject dependencyObject)
-        {
-            if (dependencyObject == null)
-                throw new ArgumentNullException(nameof(dependencyObject));
+            => (bool)dependencyObject.GetValue(UseTransitionsProperty);
 
-            return (bool)dependencyObject.GetValue(UseTransitionsProperty);
-        }
         public static void SetUseTransitions(DependencyObject dependencyObject, bool useTransitions)
             => dependencyObject.SetValue(UseTransitionsProperty, useTransitions);
+
+        internal static readonly DependencyProperty IsSubscribedProperty =
+            DependencyProperty.RegisterAttached("IsSubscribed",
+                typeof(bool),
+                typeof(Interactive),
+                new PropertyMetadata(false));
+
+        internal static bool GetIsSubscribed(DependencyObject dependencyObject)
+            => (bool)dependencyObject.GetValue(IsSubscribedProperty);
+
+        internal static void SetIsSubscribed(DependencyObject dependencyObject, bool subscribed)
+            => dependencyObject.SetValue(IsSubscribedProperty, subscribed);
+
+        public static readonly DependencyProperty VisualStateProperty =
+            DependencyProperty.RegisterAttached("VisualState",
+                typeof(string),
+                typeof(Interactive),
+                new PropertyMetadata(null, VisualStateChanged));
+
+        public static string GetVisualState(DependencyObject dependencyObject)
+            => (string)dependencyObject.GetValue(VisualStateProperty);
+
+        public static void SetVisualState(DependencyObject dependencyObject, string visualState)
+            => dependencyObject.SetValue(VisualStateProperty, visualState);
+
+        private static void VisualStateChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs eventArgs)
+        {
+            var frameworkElement = dependencyObject as FrameworkElement;
+            Debug.WriteLineIf(frameworkElement == null, "The provided DependencyObject is not a FrameworkElement.");
+
+            if (frameworkElement != null)
+            {
+                if (!GetIsSubscribed(dependencyObject))
+                {
+                    SetIsSubscribed(dependencyObject, true);
+                    frameworkElement.Loaded += FrameworkElementLoadedEventHandler;
+                }
+
+                var control = TryFindControlFrom(frameworkElement);
+                if (control != null)
+                    control.GoTo(eventArgs.NewValue as string, GetUseTransitions(frameworkElement));
+            }
+        }
+
+        private static void FrameworkElementLoadedEventHandler(object sender, RoutedEventArgs e)
+        {
+            var frameworkElement = (FrameworkElement)sender;
+            var control = TryFindControlFrom(frameworkElement);
+
+            Debug.WriteLineIf(control == null, "Could not find a control to set the visual state to.");
+
+            if (control != null)
+                control.GoTo(GetVisualState(frameworkElement), GetUseTransitions(frameworkElement));
+        }
+
+        private static Control TryFindControlFrom(FrameworkElement frameworkElement)
+        {
+            if (frameworkElement == null)
+                return null;
+
+            Control control;
+
+            do
+            {
+                control = frameworkElement as Control;
+                frameworkElement = frameworkElement.Parent as FrameworkElement;
+            } while (frameworkElement != null && control == null);
+
+            return control;
+        }
+
+        private static void GoTo(this Control control, string state, bool useTransitions)
+        {
+            if (!VisualStateManager.GoToState(control, state, useTransitions))
+                Debug.WriteLine($"Could not navigate to {state} state [SynchronizationContext: {SynchronizationContext.Current?.ToString() ?? "<null>"}].");
+        }
     }
 }
